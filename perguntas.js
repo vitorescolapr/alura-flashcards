c7978e1d0bdb92a95b0c5d24c0be24dfac823666
vitorescolapr <161034277+vitorescolapr@users.noreply.github.com>
2024-09-09 08:40:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    'O que é estudar?',
+    'É se esforçar para aprender algo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'História',
+    'Quando aconteceu a revolução francesa?',
+    'Entre 5 de maio de 1789 e 9 de novembro de 1799.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual a função do verbo To Be?',
+    'Dar sentido de ser/estar.'
+)
